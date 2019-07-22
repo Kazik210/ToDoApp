ce89a2661170169fd1f2281d2f4d1c1df0f63c09
Kazik210 <skawinau@gmail.com>
2019-07-21 20:26:22 -0400

    Little changes and added DOM Vars to Js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// DOM Variables
+const task_input = document.querySelector('.add-task__input');
+const addTask_button = document.querySelector('.add-task__button');
+const taskList_div = document.querySelector('.task-wrapper');
